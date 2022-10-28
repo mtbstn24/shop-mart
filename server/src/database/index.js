6c58c34847a12b54c229053f6e5cb2331490545b
param-ninthu <ninthu1999@gmail.com>
2022-10-28 12:34:51 +0530

    payment service

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -1,23 +1,19 @@
-const {Sequelize}  = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    'knot',
-    'postgres',
-    '0502',{
-        host:"localhost",
-        dialect:"postgres",
-    }
-);
+const sequelize = new Sequelize("shopmart", "postgres", "0502", {
+  host: "localhost",
+  dialect: "postgres",
+});
 
 sequelize.sync();
 
-(async () =>{
-    try {
+(async () => {
+  try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfuly.");}
-    catch (error){
-        console.error("Unable to connect to the database",error);
-    }
+    console.log("Connection has been established successfuly.");
+  } catch (error) {
+    console.error("Unable to connect to the database", error);
+  }
 })();
 
 module.exports = sequelize;
